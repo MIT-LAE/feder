960bb74f93d8945c3289b7cd70ff0991ff6334ea
Ian Ross <ian@skybluetrades.net>
2025-05-22 13:32:03 +0200

    Set up versioning

--- api/src/feder/__init__.py
+++ api/src/feder/__init__.py
@@ -5,3 +5,5 @@
 from .query import get_flights, flight_query  # noqa
 from .common.models import DataSource, Point, Trajectory  # noqa
 from .common.db import QueryType  # noqa
+
+__version__ = '0.1.1'
