dacff099752af1d56aaa93202020b96d208f91aa
Ian Ross <ian@skybluetrades.net>
2025-05-24 14:02:14 +0200

    Simplify Prometheus startup

--- libs/server/src/feder_server/monitoring.py
+++ libs/server/src/feder_server/monitoring.py
@@ -1,44 +1,6 @@
-from datetime import datetime, timezone, timedelta
-import logging
-import time
-
-from prometheus_client import start_http_server, Counter, Gauge
+from prometheus_client import Counter, Gauge
 
 from feder import get_feder_version
-
-
-logger = logging.getLogger(__name__)
-
-
-class PrometheusServer:
-    def __init__(self, port: int, scrape_interval: timedelta | None = None):
-        self.scrape_interval = scrape_interval or timedelta(seconds=60)
-        self.last_scrape: datetime = datetime.now(tz=timezone.utc)
-        self.server, self.thread = start_http_server(port)
-        self.server.set_app(self._scrape_wrapper(self.server.get_app()))
-
-    def shutdown(self):
-        self.server.shutdown()
-        self.thread.join()
-
-    def wait_for_scrape(self):
-        logger.info('waiting for Prometheus scrape')
-        while True:
-            now = datetime.now(tz=timezone.utc)
-            if now > self.last_scrape:
-                # Last scrape happened.
-                break
-            if (now - self.last_scrape) > 2 * self.scrape_interval:
-                # Last scrape missed.
-                logger.error('Prometheus scrape timeout')
-                break
-            time.sleep(self.scrape_interval.total_seconds() / 10)
-
-    def _scrape_wrapper(self, orig):
-        def inner(*args, **kwargs):
-            self.last_scrape = datetime.now(tz=timezone.utc)
-            return orig(*args, **kwargs)
-        return inner
 
 
 error_counter = Counter('feder_error_count', 'Feder errors', ['source'])
