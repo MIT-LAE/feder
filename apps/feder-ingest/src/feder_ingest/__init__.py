ce453f2dbf951f9893dbfe9592835377aa2d93f3
Ian Ross <ian@skybluetrades.net>
2025-06-13 12:32:31 +0200

    Bump version: 0.1.12 → 0.1.13

--- apps/feder-ingest/src/feder_ingest/__init__.py
+++ apps/feder-ingest/src/feder_ingest/__init__.py
@@ -18,7 +18,7 @@ from .db_cache import DBCache
 from .processor import Processor
 
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 
 logger = logging.getLogger(__name__)
--- apps/state-of-feder/src/state_of_feder/__init__.py
+++ apps/state-of-feder/src/state_of_feder/__init__.py
@@ -9,7 +9,7 @@ from .data import retrieve_data
 from .email import send_email
 
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 
 logger = logging.getLogger(__name__)
