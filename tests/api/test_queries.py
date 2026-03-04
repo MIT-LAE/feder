5b13efbb507af31fc976586bd0c6054a5e174a85
Ian Ross <ian@skybluetrades.net>
2026-03-04 13:47:08 +0100

    Add test for spatial filtering (currently failing)

--- tests/api/test_queries.py
+++ tests/api/test_queries.py
@@ -28,6 +28,56 @@ def test_flight_query_3():
     assert len(list(query.run())) == 0
 
 
+def test_spatial_filtering():
+    # TODO:
+    #  - Plot all flight tracks from test file.
+    #  - Identify bounding box where flight bounding box overlaps but flight
+    #    track does not.
+    #  - Test that flight is included when using bounding box that includes the
+    #    track, and excluded when using bounding box that only includes the
+    #    bounding box.
+
+    # Bounding boxes for testing: boolean value is whether any of the test
+    # flights should be returned by a spatially crossing query, following by
+    # min_lon, max_lon, min_lat, max_lat.
+    boxes = [
+        (False, -115, -110, 37, 43),
+        (True, -85, -75, 25, 30),
+        (True, -110, -70, 35, 45),
+        (False, -79, -77, 40, 41.5),
+        (False, -102, -100, 21, 31),
+        (True, -119, -113, 35, 38)
+    ]
+
+    failed = False
+    for box in boxes:
+        good, min_lon, max_lon, min_lat, max_lat = box
+        q = (
+            FlightQuery(TSTART, TEND).
+            time_starts_in().spatially_crosses().
+            with_bounds(
+                min_lon=min_lon, max_lon=max_lon,
+                min_lat=min_lat, max_lat=max_lat
+            )
+        )
+        f = list(q.run())
+        if good:
+            if len(f) == 0:
+                failed = True
+                print(
+                    'expected to find flights crossing box '
+                    f'{min_lon}, {max_lon}, {min_lat}, {max_lat}'
+                )
+        else:
+            if len(f) != 0:
+                failed = True
+                print(
+                    'unexpectedly found flights crossing box '
+                    f'{min_lon}, {max_lon}, {min_lat}, {max_lat}'
+                )
+    assert not failed, 'spatial filtering test failed'
+
+
 def test_waypoint_filtering():
     # This test uses a database file using the new schema.
     bbox = BoundingBox(min_lon=-120, max_lon=-90)
